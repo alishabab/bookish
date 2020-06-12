a4b7acf7b764e4233cf0d502505b357a03bcba5c
Shabab Ali <shababsaifi@gmail.com>
2020-06-12 16:00:21 +0530

    Fixed bug for not updating the article

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ApplicationRecord
   has_many :categorizations, dependent: :delete_all
-  has_many :articles, through: :categorizations, dependent: :destroy
+  has_many :articles, through: :categorizations, dependent: :delete_all
   validates :name, presence: true, length: { in: 2..30 }, uniqueness: true
 end
