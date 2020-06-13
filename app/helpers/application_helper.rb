38ab7f9e9c654726aaee6d616fd697435b681a26
Shabab Ali <shababsaifi@gmail.com>
2020-06-13 13:12:17 +0530

    Moved default image logic to application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
   def admin?
     current_user == User.find_by(username: 'admin')
   end
+
+  def default_img
+    'https://cdn.pixabay.com/photo/2015/10/31/14/33/dark-1015824_960_720.jpg'
+  end
 end
